bf634e3fabe3c2540cde05f35b06ed46284f4755
Manuel G <mgonzaleyub@alumnos.unex.es>
2021-07-08 11:15:59 +0200

    Añadida imagen de flor predicha

--- iris/views.py
+++ iris/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .forms import ModelForm
+import pickle
 
 # Create your views here.
 def predict_model(request):
@@ -16,7 +17,30 @@ def predict_model(request):
             petal_width = form.cleaned_data['petal_width']
 
             # give prediction response
-            prediction = "MyPrediction"
+            # Run new features through ML model
+            model_features = [
+                [sepal_length, sepal_width, petal_length, petal_width]]
+            loaded_model = pickle.load(
+                open("ml_model/iris_model.pkl", 'rb'))
+            prediction = loaded_model.predict(model_features)[0]
+
+            prediction_dict = [{'name': 'setosa',
+                                'img': 'https://alchetron.com/cdn/iris-setosa-0ab3145a-68f2-41ca-a529-c02fa2f5b02-resize-750.jpeg'},
+                                {'name': 'versicolor',
+                                'img': 'https://wiki.irises.org/pub/Spec/SpecVersicolor/iversicolor07.jpg'},
+                                {'name': 'virginica',
+                                'img': 'https://www.gardenia.net/storage/app/public/uploads/images/detail/xUM027N8JI22aQPImPoH3NtIMpXkm89KAIKuvTMB.jpeg'}]
+
+            prediction_name = prediction_dict[prediction]['name']
+            prediction_img = prediction_dict[prediction]['img']
+
+            return render(request, 'home.html', {'form': form, 'prediction': prediction,
+                                        'prediction_name': prediction_name,
+                                        'prediction_img': prediction_img})
+
+
+
+
             return render(request, 'home.html', {'form': form, 'prediction': prediction})
 
     # if a GET (or any other method) we'll create a blank form
