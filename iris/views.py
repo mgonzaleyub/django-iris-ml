5f0fe09021362045c13a71b1fd8ef8e3127e29af
Manuel G <mgonzaleyub@alumnos.unex.es>
2021-07-08 11:03:53 +0200

    Added form

--- iris/views.py
+++ iris/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
+from .forms import ModelForm
 
 # Create your views here.
-def home(request):
-    return render(request, 'home.html')
+def predict_model(request):
+# if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = ModelForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            sepal_length = form.cleaned_data['sepal_length']
+            sepal_width = form.cleaned_data['sepal_width']
+            petal_length = form.cleaned_data['petal_length']
+            petal_width = form.cleaned_data['petal_width']
+
+            # give prediction response
+            prediction = "MyPrediction"
+            return render(request, 'home.html', {'form': form, 'prediction': prediction})
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = ModelForm()
+
+    return render(request, 'home.html', {'form': form})
